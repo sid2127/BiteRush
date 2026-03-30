5c09d52ce4658d00e81714a82b0a9867ae69adcf
sid2127 <mehraprema8@gmail.com>
2026-03-30 15:02:51 +0530

    refactor: update socket connection to use centralized serverUrl import

--- Frontend/socket.js
+++ Frontend/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+import { serverUrl } from "./src/App";
 
-export const socket = io(import.meta.env.VITE_SERVER_URL, {
+export const socket = io(serverUrl, {
   withCredentials: true,
   autoConnect: false   // optional (recommended)
 });
